1164959133b4fc6dd52dbaf92283956bf7b73775
imminji000 <swlss79@naver.com>
2023-03-10 23:03:35 +0900

    p.152까지 연습

---
+++ Book1/src/classpart/Student.java
@@ -0,0 +1,24 @@
+package classpart;
+
+public class Student {
+		
+		int studentId;
+		String studentName;
+		int grade;
+		String address;
+		
+		
+		public String getStudentName() {
+			return studentName;
+		}
+		
+		public static void main(String[] args) {
+			Student studentAhn = new Student();
+			studentAhn.studentName ="안연수";
+			
+			System.out.println(studentAhn.studentName);
+			System.out.println(studentAhn.getStudentName());
+		}
+	}
+
+
